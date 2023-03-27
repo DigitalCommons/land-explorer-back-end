c984409cba863fbb91507efed16a8182e14f057a
rogup <48825436+rogup@users.noreply.github.com>
2023-03-27 15:59:57 +0100

    Remove trailing slash from auth front end route
    (cherry picked from commit c4ecb2714efe66efc2db62259523378cbc519669)

--- src/queries/mails.ts
+++ src/queries/mails.ts
@@ -2,7 +2,7 @@ import sgMail from "@sendgrid/mail";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
 
-const authRoute = "/auth/";
+const authRoute = "/auth";
 let sender = "landexplorer@digitalcommons.coop";
 let senderName = "Land Explorer";
 
