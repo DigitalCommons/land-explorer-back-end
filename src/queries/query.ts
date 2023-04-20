198592b4448b3fb2be56282b276ce3b7651e56f7
rogup <rohit5.gup@gmail.com>
2023-04-20 18:21:16 +0100

    Fix public map view function and refactor

--- src/queries/query.ts
+++ src/queries/query.ts
@@ -1,162 +1,144 @@
 import { User, Map, UserMap, UserMapAccess, PendingUserMap, polygonDbSequelize, Marker, Polygon, Line, DataGroup, DataGroupMembership, UserGroup, UserGroupMembership } from './database';
-
-const { QueryTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const helper = require('./helpers');
-const axios = require('axios').default;
+import { getMapMarkers, getMapPolygonsAndLines } from '../queries/map';
+import { hashPassword } from './helpers';
+import bcrypt from 'bcrypt';
+import { QueryTypes } from 'sequelize';
+import axios from 'axios';
 
 export const getUser = async (options = {}) => {
-  let result = await User.findOne(options);
+	let result = await User.findOne(options);
 
-  return result;
+	return result;
 }
 
-/**
- * 
- * @param id 
- * @returns 
- */
 export const getUserById = async (id: number, options: any = {}): Promise<typeof User | null> => {
-  return await User.findByPk(id, options);
+	return await User.findByPk(id, options);
 }
 
 /**
- * Check whether user with the given username exist
- * 
- * @param username 
- * @returns 
+ * Check whether user with the given username exists
  */
 export const usernameExist = async (username: string): Promise<Boolean> => {
-  return (await User.findOne({ where: { username: username } })) !== null;
+	return (await User.findOne({ where: { username: username } })) !== null;
 }
 
 /**
  * Register user with data from API request.
  * Data should already be validated.
- * 
- * @param data
- * @returns 
  */
 export const registerUser = async (data: any) => {
-  if (data.marketing) {
-    axios.post("https://api.buttondown.email/v1/subscribers", {
-      email: data.username,
-      referrer_url: "https://app.landexplorer.coop/register"
-    }, {
-      headers: {
-        'Authorization': `Token ${process.env.BUTTONDOWN_API_KEY}`
-      }
-    })
-  }
+	if (data.marketing) {
+		axios.post("https://api.buttondown.email/v1/subscribers", {
+			email: data.username,
+			referrer_url: "https://app.landexplorer.coop/register"
+		}, {
+			headers: {
+				'Authorization': `Token ${process.env.BUTTONDOWN_API_KEY}`
+			}
+		})
+	}
 
-  return await User.create({
-    username: data.username,
-    password: helper.hashPassword(data.password),
-    enabled: 1,
-    access: 2,
-    is_super_user: 0,
-    first_name: data.firstName,
-    last_name: data.lastName,
-    address1: data.address1,
-    address2: data.address2,
-    postcode: data.postcode,
-    phone: data.phone,
-    organisation_number: data.organisationNumber,
-    organisation: data.organisation,
-    organisation_activity: data.organisationSubType,
-    organisation_type: data.organisationType,
-    marketing: data.marketing,
-    council_id: data.username.endsWith("rbkc.gov.uk") ? 1 : 0
-  });
+	return await User.create({
+		username: data.username,
+		password: hashPassword(data.password),
+		enabled: 1,
+		access: 2,
+		is_super_user: 0,
+		first_name: data.firstName,
+		last_name: data.lastName,
+		address1: data.address1,
+		address2: data.address2,
+		postcode: data.postcode,
+		phone: data.phone,
+		organisation_number: data.organisationNumber,
+		organisation: data.organisation,
+		organisation_activity: data.organisationSubType,
+		organisation_type: data.organisationType,
+		marketing: data.marketing,
+		council_id: data.username.endsWith("rbkc.gov.uk") ? 1 : 0
+	});
 }
 
 /**
  * Before a user is registered, other existing user may have shared a map to this user.
  * These map are stored in 'pending_user_map'. Now that a given user is registered,
  * we migrate the map to 'user_map'
- * 
- * @param user 
- * @returns 
  */
 export const migrateGuestUserMap = async (user: typeof User) => {
 
-  try {
+	try {
 
-    const userMapData = (await PendingUserMap
-      .findAll({
-        where: {
-          email_address: user.username
-        }
-      }))
-      // map to format ready to be inserted to user_map table
-      .map(function (pendingUserMap: any) {
-        return {
-          access: UserMapAccess.Readonly,
-          viewed: 0,
-          map_id: pendingUserMap.map_id,
-          user_id: user.id
-        }
-      });
+		const userMapData = (await PendingUserMap
+			.findAll({
+				where: {
+					email_address: user.username
+				}
+			}))
+			// map to format ready to be inserted to user_map table
+			.map(function (pendingUserMap: any) {
+				return {
+					access: UserMapAccess.Readonly,
+					viewed: 0,
+					map_id: pendingUserMap.map_id,
+					user_id: user.id
+				}
+			});
 
-    // bulk create the user map
-    await UserMap.bulkCreate(userMapData);
+		// bulk create the user map
+		await UserMap.bulkCreate(userMapData);
 
-    // Now delete user map from pendingUserMap
-    await PendingUserMap.destroy({
-      where: {
-        email_address: user.username
-      }
-    });
+		// Now delete user map from pendingUserMap
+		await PendingUserMap.destroy({
+			where: {
+				email_address: user.username
+			}
+		});
 
-  } catch (error: any) {
-    console.log(error.message);
-  }
+	} catch (error: any) {
+		console.log(error.message);
+	}
 }
 
 
 /**
  * Return the user if they exist and the password matches, otherwise return false.
  */
-export async function checkAndReturnUser(username: string, password: string): Promise<typeof User | false> {
+export const checkAndReturnUser = async (username: string, password: string): Promise<typeof User | false> => {
+	const user = await getUser({ where: { username: username }, raw: true });
 
-  const user = await getUser({ where: { username: username }, raw: true });
+	if (!user) {
+		return false;
+	}
 
-  if (!user) {
-    return false;
-  }
+	const match = await bcrypt.compare(password, user.password);
 
-  const match = await bcrypt.compare(password, user.password);
+	if (match) {
+		return user;
+	}
 
-  if (match) {
-    return user;
-  }
-
-  return false;
+	return false;
 }
 
 /**
  * Return the geojson polygons of land ownership within a given bounding box area 
- * @param username 
- * @param password 
- * @returns User
  */
-export async function getPolygon(sw_lng: number, sw_lat: number, ne_lng: number, ne_lat: number) {
+export const getPolygon = async (sw_lng: number, sw_lat: number, ne_lng: number, ne_lat: number) => {
 
-  const x1 = sw_lng;
-  const y1 = sw_lat;
+	const x1 = sw_lng;
+	const y1 = sw_lat;
 
-  const x2 = ne_lng;
-  const y2 = sw_lat;
+	const x2 = ne_lng;
+	const y2 = sw_lat;
 
-  const x3 = ne_lng;
-  const y3 = ne_lat;
+	const x3 = ne_lng;
+	const y3 = ne_lat;
 
-  const x4 = sw_lng;
-  const y4 = ne_lat;
+	const x4 = sw_lng;
+	const y4 = ne_lat;
 
-  const searchArea = "POLYGON ((" + x1 + " " + y1 + ", " + x2 + " " + y2 + ", " + x3 + " " + y3 + ", " + x4 + " " + y4 + ", " + x1 + " " + y1 + "))";
+	const searchArea = "POLYGON ((" + x1 + " " + y1 + ", " + x2 + " " + y2 + ", " + x3 + " " + y3 + ", " + x4 + " " + y4 + ", " + x1 + " " + y1 + "))";
 
-  const query = `SELECT 
+	const query = `SELECT 
   lop.id as id, 
   lop.poly_id as poly_id, 
   lop.title_no as title_no, 
@@ -205,168 +187,224 @@ export async function getPolygon(sw_lng: number, sw_lat: number, ne_lng: number,
   ON lop.title_no = lo.title_no
   WHERE ST_Intersects(lop.geom, ST_GeomFromText("` + searchArea + `"))`;
 
-  return await polygonDbSequelize.query(query, { type: QueryTypes.SELECT });
+	return await polygonDbSequelize.query(query, { type: QueryTypes.SELECT });
 }
 
-export async function findAllDataGroupContentForUser(userId: number) {
-  const userGroupMemberships = await UserGroupMembership.findAll({
-    where: {
-      user_id: userId
-    }
-  });
+export const findAllDataGroupContentForUser = async (userId: number) => {
+	const userGroupMemberships = await UserGroupMembership.findAll({
+		where: {
+			user_id: userId
+		}
+	});
 
-  const userGroups: any[] = [];
+	const userGroups: any[] = [];
 
-  for (let membership of userGroupMemberships) {
-    userGroups.push(await UserGroup.findOne({
-      where: {
-        iduser_groups: membership.user_group_id
-      }
-    }))
-  }
+	for (let membership of userGroupMemberships) {
+		userGroups.push(await UserGroup.findOne({
+			where: {
+				iduser_groups: membership.user_group_id
+			}
+		}))
+	}
 
-  const userGroupsAndData: any[] = [];
+	const userGroupsAndData: any[] = [];
 
-  for (let group of userGroups) {
-    const dataGroupMemberships = await DataGroupMembership.findAll({
-      where: {
-        user_group_id: group.iduser_groups
-      }
-    })
+	for (let group of userGroups) {
+		const dataGroupMemberships = await DataGroupMembership.findAll({
+			where: {
+				user_group_id: group.iduser_groups
+			}
+		})
 
-    let dataGroups: any[] = [];
+		let dataGroups: any[] = [];
 
-    for (let membership of dataGroupMemberships) {
-      dataGroups = dataGroups.concat(
-        await DataGroup.findAll({
-          where: {
-            iddata_groups: membership.data_group_id
-          }
-        })
-      )
-    }
+		for (let membership of dataGroupMemberships) {
+			dataGroups = dataGroups.concat(
+				await DataGroup.findAll({
+					where: {
+						iddata_groups: membership.data_group_id
+					}
+				})
+			)
+		}
 
-    for (let dataGroup of dataGroups) {
-      dataGroup.dataValues.markers = await Marker.findAll({
-        where: {
-          data_group_id: dataGroup.iddata_groups
-        }
-      });
-      dataGroup.dataValues.polygons = await Polygon.findAll({
-        where: {
-          data_group_id: dataGroup.iddata_groups
-        }
-      });
-      dataGroup.dataValues.lines = await Line.findAll({
-        where: {
-          data_group_id: dataGroup.iddata_groups
-        }
-      });
-    }
+		for (let dataGroup of dataGroups) {
+			dataGroup.dataValues.markers = await Marker.findAll({
+				where: {
+					data_group_id: dataGroup.iddata_groups
+				}
+			});
+			dataGroup.dataValues.polygons = await Polygon.findAll({
+				where: {
+					data_group_id: dataGroup.iddata_groups
+				}
+			});
+			dataGroup.dataValues.lines = await Line.findAll({
+				where: {
+					data_group_id: dataGroup.iddata_groups
+				}
+			});
+		}
 
-    userGroupsAndData.push({
-      name: group.name,
-      id: group.iduser_groups,
-      dataGroups
-    })
-  }
+		userGroupsAndData.push({
+			name: group.name,
+			id: group.iduser_groups,
+			dataGroups
+		})
+	}
 
-  return userGroupsAndData;
+	return userGroupsAndData;
 }
 
-export async function hasAccessToDataGroup(userId: number, dataGroupId: number) {
-  const userGroupMemberships = await UserGroupMembership.findAll({
-    where: {
-      user_id: userId
-    }
-  });
+export const hasAccessToDataGroup = async (userId: number, dataGroupId: number): Promise<boolean> => {
+	const userGroupMemberships = await UserGroupMembership.findAll({
+		where: {
+			user_id: userId
+		}
+	});
 
-  for (let membership of userGroupMemberships) {
-    const dataGroupMembership = await DataGroupMembership.findOne({
-      where: {
-        user_group_id: membership.user_group_id,
-        data_group_id: dataGroupId
-      }
-    });
+	for (let membership of userGroupMemberships) {
+		const dataGroupMembership = await DataGroupMembership.findOne({
+			where: {
+				user_group_id: membership.user_group_id,
+				data_group_id: dataGroupId
+			}
+		});
 
-    if (dataGroupMembership) {
-      return true;
-    }
-  }
+		if (dataGroupMembership) {
+			return true;
+		}
+	}
 
-  return false;
+	return false;
 }
 
-/* Queries for the public map views  */
+/**
+ * Get a GeoJSON feature collection containing all the markers, polys and lines in a map, including
+ * those in active data groups.
+ */
+export const getGeoJsonFeaturesForMap = async (mapId: number) => {
+	const map = await Map.findOne({
+		where: {
+			id: mapId
+		}
+	});
+	const mapData = JSON.parse(map.data);
 
-const publicUserId = -1;
+	// Get markers, polygons and lines that are saved to the map
+	const markers = await getMapMarkers(mapId);
+	const markerFeatures = markers.map((marker: any) => ({
+		type: "Feature",
+		geometry: {
+			type: "Point",
+			coordinates: marker.coordinates
+		},
+		properties: {
+			name: marker.name,
+			description: marker.description,
+			group: 'My Drawings'
+		}
+	}));
 
-export async function findPublicMap(mapId: number) {
-  const publicMapView = await UserMap.findOne({
-    where: {
-      map_id: mapId,
-      user_id: publicUserId
-    }
-  })
+	const polygonsAndLines = await getMapPolygonsAndLines(mapId);
+	const polygonAndLineFeatures = polygonsAndLines.map((polygon: any) => ({
+		...polygon.data,
+		properties: {
+			name: polygon.name,
+			description: polygon.description,
+			group: 'My Drawings'
+		}
+	}));
 
-  if (publicMapView) {
-    const publicMap = await Map.findOne({
-      where: {
-        id: mapId
-      }
-    });
+	// Get features from datagroup layers which are active
+	const dataGroupFeatures: any[] = [];
+	for (const dataGroupId of mapData.mapLayers.myDataLayers) {
+		const dataGroup = await DataGroup.findOne({
+			where: {
+				// In old maps, myDataLayers used to be array of objects, each with an iddata_groups
+				// field. In newer maps, myDataLayers is just an array of data group IDs.
+				iddata_groups: dataGroupId.iddata_groups || dataGroupId
+			}
+		});
+		const dataGroupMarkers = await Marker.findAll({
+			where: {
+				data_group_id: dataGroupId.iddata_groups || dataGroupId
+			}
+		});
+		const dataGroupPolygons = await Polygon.findAll({
+			where: {
+				data_group_id: dataGroupId.iddata_groups || dataGroupId
+			}
+		});
+		const dataGroupLines = await Line.findAll({
+			where: {
+				data_group_id: dataGroupId.iddata_groups || dataGroupId
+			}
+		});
 
-    publicMap.data = JSON.parse(publicMap.data);
+		dataGroupMarkers.forEach((marker: any) => {
+			dataGroupFeatures.push({
+				type: "Feature",
+				geometry: marker.location,
+				properties: {
+					name: marker.name,
+					description: marker.description,
+					group: dataGroup.title
+				}
+			})
+		});
+		dataGroupPolygons.forEach((polygon: any) => {
+			dataGroupFeatures.push({
+				type: "Feature",
+				geometry: polygon.vertices,
+				properties: {
+					name: polygon.name,
+					description: polygon.description,
+					group: dataGroup.title
+				}
+			})
+		});
+		dataGroupLines.forEach((line: any) => {
+			dataGroupFeatures.push({
+				type: "Feature",
+				geometry: line.vertices,
+				properties: {
+					name: line.name,
+					description: line.description,
+					group: dataGroup.title
+				}
+			})
+		});
+	}
 
-    for (let dataGroup of publicMap.data.mapLayers.myDataLayers) {
-      dataGroup.markers = await Marker.findAll({
-        where: {
-          data_group_id: dataGroup.iddata_groups
-        }
-      });
-    }
-
-    return publicMap;
-  }
-  else
-    return "No public map at this address."
+	return {
+		type: "FeatureCollection",
+		features: [...markerFeatures, ...polygonAndLineFeatures, ...dataGroupFeatures]
+	};
 }
 
-export async function createPublicMapView(mapId: number, userId: number) {
-  const userMapView = await UserMap.findOne({
-    where: {
-      map_id: mapId,
-      user_id: userId
-    }
-  });
+/**
+ * Make a specified map available to the public.
+ * @returns URI for the public map view
+ */
+export const createPublicMapView = async (mapId: number): Promise<string> => {
+	const publicUserId = -1;
 
-  if (userMapView.access === UserMapAccess.Readwrite) {
-    const publicViewExists = await UserMap.findOne({
-      where: {
-        map_id: mapId,
-        user_id: publicUserId,
-      }
-    });
+	const publicViewExists = await UserMap.findOne({
+		where: {
+			map_id: mapId,
+			user_id: publicUserId,
+		}
+	});
 
-    if (!publicViewExists) {
-      await UserMap.create({
-        map_id: mapId,
-        user_id: publicUserId,
-        access: UserMapAccess.Readonly,
-        viewed: 0
-      });
-    }
+	if (!publicViewExists) {
+		await UserMap.create({
+			map_id: mapId,
+			user_id: publicUserId,
+			access: UserMapAccess.Readonly,
+			viewed: 0
+		});
+	}
 
-    return {
-      success: true,
-      message: "Made map public",
-      URI: `/api/public/map/${mapId}`
-    };
-  }
-  else {
-    return {
-      success: false,
-      message: "You don't have write access to this map, so can't make it public."
-    };
-  }
+	return `/api/public/map/${mapId}`;
 }
