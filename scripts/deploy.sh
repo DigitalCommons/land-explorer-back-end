d4daf6c6eece94a6ef22950a185322856938a0b7
rogup <rohit5.gup@gmail.com>
2023-05-11 18:44:55 +0100

    Add GitHub Action and improve deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+# Run this script on the server to pull the latest code and deploy it.
+
 set -e
 set -x
+
+# If branch was inputted as argument, check it is checked out
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ ! -z "$1" ] && [ $1 != $CURRENT_BRANCH ]; then
+    echo "ERROR: We cannot deploy branch $1 because the branch $CURRENT_BRANCH is checked out"
+    exit 1
+fi
 
 # Pull latest code
 git pull
