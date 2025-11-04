cf135772daa97705b377200cb010759504d6083d
John Evans <38507954+King-Mob@users.noreply.github.com>
2025-11-04 14:53:08 +0000

    add deduplication

--- migrations/2025101314000000-add-uuid-unique-indexes.js
+++ migrations/2025101314000000-add-uuid-unique-indexes.js
@@ -2,6 +2,25 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const tables = ["markers", "polygons", "linestrings"];
+
+    for (const table of tables) {
+      await queryInterface.sequelize.query(`
+        UPDATE ${table}
+        SET uuid = UUID()
+        WHERE uuid IS NULL OR uuid = '';
+      `);
+
+      // Replace duplicate UUIDs with new ones
+      await queryInterface.sequelize.query(`
+        UPDATE ${table} t1
+        JOIN ${table} t2
+          ON t1.uuid = t2.uuid
+         AND t1.id > t2.id
+        SET t1.uuid = UUID();
+      `);
+    }
+
     await queryInterface.sequelize.query(`
       ALTER TABLE markers
       ADD UNIQUE KEY markers_uuid_unique (uuid);
